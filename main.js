455964534ba49cc12e4eb9f2d4621dae5c1646b3
HARIS AHMED <136917000+harisahmedjs@users.noreply.github.com>
2024-01-23 00:12:28 +0500

    completed

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ setInterval(()=>{
     let hours = currentDate.getHours();
     
     
-    hour.innerHTML = (hours<10 ? "0" : "") + (hours%12 || 12);
+    hour.innerHTML = (hours % 12 || 12) < 10 ? "0" + (hours % 12 || 12) : (hours % 12 || 12);
     minute.innerHTML = (currentDate.getMinutes()<10 ? "0" :"") + currentDate.getMinutes();
     second.innerHTML = (currentDate.getSeconds()<10 ? "0" :"") + currentDate.getSeconds();
 },1000)
